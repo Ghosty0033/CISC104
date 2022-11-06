28e8a233714cd522071f813409deaf0d37a43a21
Ghosty <113139868+Ghosty0033@users.noreply.github.com>
2022-11-05 21:19:15 -0400

    Add files via upload

---
+++ GreenBallCollisionManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class GreenBallCollisionManager : BallCollisionManager
+{
+    public GameObject GreenBall;
+    public override void CollideWithBall(GameObject OtherBall)
+    { 
+        Debug.Log("Green Ball Collision Manager Function");
+
+        
+
+        // Green Ball turns white
+        var GreenBallRenderer = GreenBall.GetComponent<Renderer>();
+         GreenBallRenderer.material.SetColor("_Color", Color.white);
+    }
+}
+
---
+++ RedBallCollisionManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public class RedBallCollisionManager : BallCollisionManager
+{
+    public GameObject RedBall;
+    public override void CollideWithBall(GameObject OtherBall)
+    {
+        Debug.Log("Red Ball Collision Manager Function");
+
+        //Red Ball decreases in size
+        RedBall.transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+        
+    }
+}
+
